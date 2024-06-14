414c2caff6a2f60344a335d020312ee4a5841ddb
NicoADominguez <98nicodominguez@gmail.com>
2024-06-14 12:24:15 -0400

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>You got the travel plans, we got the travel vans.</h1>
+      <h1>I like Cookies!</h1>
       <p>
         Add adventure to your life by joining the #vanlife movement. Rent the
         perfect van to make your perfect road trip.
