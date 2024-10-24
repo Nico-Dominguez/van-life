83b45babd7edf9acdc8e5f408adacd7be54ec868
NicoADominguez <98nicodominguez@gmail.com>
2024-10-23 23:32:41 -0400

    about page ui refactor

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="footer">Copyright 2024 VanLife</div>;
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,62 @@
-import React from "react";
+// Home.jsx
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  return (
-    <div className="home-container">
-      <div className="home-one">
-        <h1>Go on your next adventure in comfort!</h1>
-        <p>
-          Add adventure to your life by joining the #vanlife movement. Rent the
-          perfect van to make your perfect road trip.
-        </p>
-        <Link to="vans">Find your van</Link>
-      </div>
-      <div className="home-two"></div>
+  const [featuredVans, setFeaturedVans] = useState([]);
+
+  useEffect(() => {
+    async function fetchFeaturedVans() {
+      try {
+        const response = await fetch("/api/vans");
+        const data = await response.json();
+        // Get only the first 3 vans
+        setFeaturedVans(data.vans.slice(0, 3));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchFeaturedVans();
+  }, []);
+
+  const featuredVanElements = featuredVans.map((van) => (
+    <div key={van.id} className="featured-van-tile">
+      <Link
+        to={`/vans/${van.id}`}
+        aria-label={`View details for ${van.name}, priced at $${van.price} per day`}
+      >
+        <img src={van.imageUrl} alt={`Image of ${van.name}`} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
     </div>
+  ));
+
+  return (
+    <>
+      <div className="home-container">
+        <div className="home-one">
+          <h1>Go on your next adventure in comfort!</h1>
+          <p>
+            Add adventure to your life by joining the #vanlife movement. Rent
+            the perfect van to make your perfect road trip.
+          </p>
+          <Link to="vans">Find your van</Link>
+        </div>
+        <div className="home-two"></div>
+      </div>
+
+      <div className="featured-vans-container">
+        <h2>Featured Vans</h2>
+        <div className="featured-vans-grid">{featuredVanElements}</div>
+      </div>
+    </>
   );
 }
