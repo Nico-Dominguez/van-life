6900c0408fac721b91a0f0d68bd5a5959a3452f4
NicoADominguez <98nicodominguez@gmail.com>
2024-10-23 23:06:06 -0400

    fix home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>I like Cookies!</h1>
-      <p>
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
-      </p>
-      <Link to="vans">Find your van</Link>
+      <div className="home-one">
+        <h1>Go on your next adventure in comfort!</h1>
+        <p>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <Link to="vans">Find your van</Link>
+      </div>
+      <div className="home-two"></div>
     </div>
   );
 }
